a109b371f1f4b6d806810f478f60aa2d52958644
SYN <postbox.501@yandex.com>
2023-05-14 03:56:31 +0530

    loops in c language

---
+++ for-loop-in-c.c
@@ -0,0 +1,34 @@
+
+// for (initialise counter; lest counter; increment counter)
+// {
+//      do this;
+//      and this;
+// }
+
+
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     // calculation of simple interest for 3 sets of p, n and r
+
+//     int p, n, count;
+//     float r, si;
+
+//     for (count = 1; count <= 3; count++)
+//     {
+//         printf("Enter values of p, n, and r : ");
+//         scanf("%d %d %f", &p, &n, &r);
+
+//         si = p * n * r / 100;
+
+//         printf("Simple intrest is : Rs.%f\n", si);
+//     }
+    
+//     return 0;
+
+// }
+
+
+
